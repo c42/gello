842bc1f116588ff97983295a69b272bcacaf6b52
Niranjan Paranjape <niranjangp@gmail.com>
2013-09-14 14:45:58 +0530

    Init with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gello::Application.config.secret_token = 'f7b372c3473d4bb435f5ad94175f5ec91b0a24a45c302c0ebcfbe9b6509749cbc5cfd4f99016ec54d4e30133692681d2b5d11558fc2e8eb209d091cdf8acb19d'

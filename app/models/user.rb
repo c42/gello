abdfa631671f7f5d0fcb9f284a547913f934ec2a
Sidu Ponnappa <ckponnappa@gmail.com>
2013-09-14 17:55:05 +0530

    Login/Logout flow complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :omniauthable, :rememberable
+  devise :omniauthable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :provider, :uid
-  # attr_accessible :title, :body
   
   def self.find_or_create_from_oauth(auth)
     record = where(provider: auth.provider, uid: auth.uid.to_s).first
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Gello::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  
   root :to => "home#index"
   
   # The priority is based upon order of creation:

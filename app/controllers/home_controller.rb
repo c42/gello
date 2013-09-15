5a11034747edb62c6f7f0462b964884e68396b50
Niranjan Paranjape <niranjangp@gmail.com>
2013-09-15 15:44:21 +0530

    reverting the ui changes for the home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
-  def index    
-    redirect_to "/c42/wrest" if user_signed_in?
+  def index
+    if user_signed_in?
+      user = current_user_with_access_token
+      repos = user.all_repos_with_issues
+      @managed, @unmanaged = repos.partition {|r| Project.where(github_path: r.full_name).exists?}
+      render "repositories", layout: "application_internal"
+    else
+      render "landing_page"
+    end
   end
 end
